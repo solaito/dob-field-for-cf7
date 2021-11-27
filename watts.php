1688bc9a0bfe7cdb733ed5b163ee7e6c313cd757
s.matsuura <satoshi.matsuura@tonica.llc>
2021-11-27 22:54:43 +0900

    v1.1.2へのバージョンアップ

--- watts.php
+++ watts.php
@@ -7,7 +7,7 @@
  * Author URI:      https://github.com/solaito/
  * Text Domain:     watts
  * Domain Path:     /languages
- * Version:         1.1.1
+ * Version:         1.1.2
  *
  * @package         Watts
  */
